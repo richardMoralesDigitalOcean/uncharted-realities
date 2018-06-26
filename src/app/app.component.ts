88f4976bf8e36a8a50e0431fd56bcfdd4e1849b0
richardMoralesDigitalOcean <31902448+richardMoralesDigitalOcean@users.noreply.github.com>
2018-06-26 13:45:04 -0400

    Bug Fixes
    + Fixed bug where sideNav would display wrong color and have wrong element active on reload
    + Added feature where sideNav state is remembered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as $ from 'jquery';
 
@@ -10,6 +11,8 @@ import * as $ from 'jquery';
 export class AppComponent implements OnInit {
   title = 'app';
   menuState = false;
+  route: string;
+  href = '';
   /*backgroundUrls: any = {
     home: 'assets/Images/backgrounds/abstract_polygonal/abstract_polygonal_black.PNG',
     worlds: 'assets/Images/backgrounds/abstract_polygonal/abstract_polygonal_dark_blue.PNG',
@@ -25,21 +28,91 @@ export class AppComponent implements OnInit {
     about: 'white',
     contact: '#faed26'
   };
-  constructor(private _route: ActivatedRoute, private router: Router) {
+  constructor(private _route: ActivatedRoute, private router: Router, private location: Location) {
   }
   ngOnInit() {
-    $('.navContainer').hide();
+    this.initSideNav(this.location);
   }
+  // Set proper menu link to active upon reload
+  initSideNav(location: Location): void {
+    /*
+      + Get corresponding sideNav element base on initial path
+      + If location is empty, default to home
+      + This will ensure the proper sideNav element is active and
+        displays the left-border
+    */
+    let loc = this.location.path();
+    if (loc === '') {
+      loc = 'home';
+    }
+    const path = loc.substring(1);
+    const elem = $(`#${path}`);
+    /*
+      + Set corresponding sideNav element to active
+      + Set border color
+    */
+    elem.addClass('active');
+    elem.css('border-left', `2px solid ${this.colorSchemes[path]}`);
+    /*
+      + SideNav State Check
+      + First Check if 'menuState' exists in sessionStorage
+        to prevent null errors for users navigating to site from another site
+      + storedState will either be 'true' or 'false
+      + storedState used to set menuState component variable as well as
+        show or hide sideNav component
+    */
+    const storedState = sessionStorage.getItem('menuState');
+    if (storedState) {
+      switch (storedState) {
+        case 'true':
+          this.menuState = true;
+          $('.navContainer').show();
+          break;
+        default:
+          this.menuState = false;
+          $('.navContainer').hide();
+          break;
+      }
+    } else {
+      sessionStorage.setItem('menuState', this.menuState.toString());
+    }
+  }
+  /*
+  ensureProperNavigation(router: Router, location: Location): void {
+   router.events.subscribe((val) => {
+     switch (location.path()) {
+      case '': {
+        this.route = 'home';
+        break;
+      }
+      default: {
+        if()
+      }
+     }
+      if (location.path() !== '') {
+        this.route = location.path();
+      } else {
+        this.route = 'home';
+
+        // self.router.navigate(['/SignIn'])
+      }
+    });
+  }*/
   navigation(event): void {
+    /*
+      + Get the clicked sideNav element and extract id attribute to get new path
+      + Loop through all sideNav elements
+      + For all sideNav elements whose id != path, remove 'active' class and set to default style
+      + For sideNav element whose id === path, add 'active' class and set 'active' style
+    */
     const target = event.target;
-    const pageId = target.attributes.id.nodeValue;
+    const path = target.attributes.id.nodeValue;
     const elements = $('.appContainer a.nav-link');
     for (const elem of elements) {
       const currId = $(elem).attr('id');
-      console.log('Current Id: ' + currId);
-      if (currId === pageId) {
+      if (currId === path) {
         $(elem).addClass('active');
-        $(elem).css('border-left', `2px solid ${this.colorSchemes[pageId]}`);
+        $(elem).css('border-left', `2px solid ${this.colorSchemes[path]}`);
       } else {
         $(elem).removeClass('active');
         $(elem).css('border-left', `2px solid #6b6e70`);
@@ -47,12 +120,18 @@ export class AppComponent implements OnInit {
     }
   }
   toggleMenu(): void {
-    // nav menu showing if menuState is true
+    /*
+      + Check current state of menu, show or hide accordingly
+      + Update state of sessionStorage('menuState')
+      + true: sideNav is currently open
+      + false: sideNav not currently open
+    */
     if (this.menuState) {
       $('.navContainer').hide();
     } else {
       $('.navContainer').show();
     }
     this.menuState = !this.menuState;
+    sessionStorage.setItem('menuState', this.menuState.toString());
   }
 }
--- src/app/components/Home/home.component.ts
+++ src/app/components/Home/home.component.ts
@@ -8,15 +8,15 @@ import * as $ from 'jquery';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // public carouselOne: NguCarousel;
+   public carouselOne: NguCarousel;
 
   constructor() {  }
   ngOnInit() {
-    /*this.carouselOne = {
+    this.carouselOne = {
       grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
       slide: 1,
       speed: 400,
-      interval: 4000,
+      interval: 10000,
       point: {
         visible: true
       },
@@ -24,7 +24,7 @@ export class HomeComponent implements OnInit {
       touch: true,
       loop: true,
       custom: 'banner'
-    };*/
+    };
   }
   public myfunc(event: Event) {
     // carouselLoad will trigger this funnction when your load value reaches
